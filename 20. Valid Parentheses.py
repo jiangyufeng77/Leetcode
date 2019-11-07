96f541e36cda7326a78f9136120af6ce7af9f234
jiangyufeng77 <jiangyufeng77@163.com>
2019-11-07 10:03:21 +0800

    有效的括号

---
+++ 20. Valid Parentheses.py
@@ -0,0 +1,42 @@
+
+class Solution(object):
+    def isValid(self, s):
+        self.stack = []
+        for i in s:
+            if i == '{' or i =='[' or i =='(':
+                self.stack.append(i)
+            elif i == '}':
+                if len(self.stack) == 0:
+                    return False
+                elif self.stack[-1] == '{':
+                    self.stack.pop()
+                    continue
+                else:
+                    return False
+            elif i == ']':
+                if len(self.stack) == 0:
+                    return False
+                elif self.stack[-1] == '[':
+                    self.stack.pop()
+                    continue
+                else:
+                    return False
+            elif i == ')':
+                if len(self.stack) == 0:
+                    return False
+                elif self.stack[-1] == '(':
+                    self.stack.pop()
+                    continue
+                else:
+                    return False
+
+        if len(self.stack) > 0:
+            return False
+        else:
+            return True
+
+stack1 = Solution()
+str1 = '{{'
+stack = stack1.isValid(str1)
+print(stack)
+
